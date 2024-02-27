992d7d4a521fb715af980729d84a0805f7ced50c
GiiO <giiomy5@gmail.com>
2024-02-27 15:53:31 -0500

    arrowFunction

---
+++ index.js
@@ -0,0 +1,23 @@
+/* SIN PARAMETROS */
+const saludar = () => {
+    console.log("Hi");
+};
+
+saludar();
+
+
+/* UN PARAMETRO*/
+const duplicar= (num) =>{
+    return num*2;
+};
+
+console.log(duplicar(5));
+
+
+/* tres parametros */
+const sumarNumeros = (num1, num2,num3) =>{
+    return num1+ num2 +num3;
+};
+
+console.log(sumarNumeros(11,5,14));
+
